ce135e86b3d979caaf6db1b39bd7530a302261a6
Piotr Szymura <undernotic@gmail.com>
2017-01-22 19:10:21 +0100

    updated pm2 script

--- server/pm2.process.config.js
+++ server/pm2.process.config.js
@@ -2,7 +2,8 @@ module.exports = {
     apps: [
         {
             name: "home28-raspberrypi-server",
-            script: "./index.js",
+            script: "./app/index.js",
+            node_args: "--harmony-async-await",
             "instances": "max",
             "exec_mode": "cluster",
             watch: true,
