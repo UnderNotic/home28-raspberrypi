900ca2970b3d149eb71a1bb3aa19dc81155af053
undernotic <undernotic@gmail.com>
2017-01-31 21:57:02 +0100

    working font-awesome

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../css/bootstrap.css';
+import '../css/font-awesome.css';
 import '../css/main.css';
 import { AppContainer } from 'react-hot-loader';
 import Login from './containers/login/login';
